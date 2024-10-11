73272893d2fbe41a33329fa72e7b82e6426743d1
Watanabe <t22cs055@yamanashi.ac.jp>
2024-10-11 15:19:58 +0900

    hello.pyの作成

---
+++ HelloPython/hello.py
@@ -0,0 +1,14 @@
+print("Hello, Python world!")
+
+DATA = [6, 15, 4, 2, 8, 5, 11, 9, 7, 13]
+
+from quick_sort import quick_sort
+
+DATA = [5,4,3,2,1]
+
+print(quick_sort(DATA))
+
+from janken import janken
+
+janken()
+
